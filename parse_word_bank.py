ac235463300fb403860c928fd315b2356c0d54a6
ku-dmitry <akinc@yandex.ru>
2022-02-17 20:58:55 +0300

    created basic structure to word bank parser

--- parse_word_bank.py
+++ parse_word_bank.py
@@ -1,5 +1,34 @@
 import csv
 from tkinter import filedialog
+import noun_toolkit
+import logging
+
+
+def generate_nouns(input_row):
+    print("It is a noun")
+    noun_toolkit.generate_plural(input_row)
+
+
+def generate_verbs(input_row):
+    print("It is a verb")
+
+
+def generate_adjectives(input_row):
+    print("It is an adjective")
+
+
+def determine_part_of_speech(input_row):
+    if input_row['partOfSpeech'] == 'n.':
+        generate_nouns(input_row)
+    if input_row['partOfSpeech'] == 'v.':
+        generate_verbs(input_row)
+    if input_row['partOfSpeech'] == 'adj.':
+        generate_adjectives(input_row)
+
 
 input_csv = filedialog.askopenfile(mode='r')
+with open(input_csv.name, newline='', encoding='windows-1251') as csvfile:
+    word_reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+    for row in word_reader:
+        determine_part_of_speech(row)
 
