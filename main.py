51e8b704273d931e965316330dc4dc1a9a67753f
ku-dmitry <akinc@yandex.ru>
2022-02-24 21:53:56 +0300

    refactor + input language selection added

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 #! /usr/bin/env python
-import parse_word_bank
+import ru_parse_word_bank
+import csv
+from tkinter import filedialog
 
-parse_word_bank
+
+def ask_for_file():
+    print("Select basic vocabulary:")
+    return filedialog.askopenfile(mode='r')
+
+
+def select_language():
+    print("What's in the file?")
+    print("ru - Russian")
+    print("No other languages available yet")
+    language_choice = input("Choose language: ")
+    if language_choice == "ru":
+        with open(file.name, newline='', encoding='windows-1251') as csvfile:
+            word_reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+            for row in word_reader:
+                ru_parse_word_bank.determine_part_of_speech(row)
+    else:
+        print("Are you sure? Try again")
+        select_language()
+
+
+file = ask_for_file()
+select_language()
--- ru_parse_word_bank.py
+++ ru_parse_word_bank.py
@@ -1,7 +1,4 @@
-import csv
-from tkinter import filedialog
 import noun_toolkit
-import logging
 
 
 def generate_nouns(input_row):
@@ -10,11 +7,11 @@ def generate_nouns(input_row):
 
 
 def generate_verbs(input_row):
-    print("It is a verb")
+    print(input_row['ru'] + " is a verb")
 
 
 def generate_adjectives(input_row):
-    print("It is an adjective")
+    print(input_row['ru'] + " is an adjective")
 
 
 def determine_part_of_speech(input_row):
@@ -26,9 +23,5 @@ def determine_part_of_speech(input_row):
         generate_adjectives(input_row)
 
 
-input_csv = filedialog.askopenfile(mode='r')
-with open(input_csv.name, newline='', encoding='windows-1251') as csvfile:
-    word_reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
-    for row in word_reader:
-        determine_part_of_speech(row)
-
+def start(input_row):
+    determine_part_of_speech(input_row)
